4f24b9b94d518e059b168cab51a788660a35ece2
Ebony Plummer <ebony.plummer@gmail.com>
2018-01-23 00:04:57 -0500

    Updated the app.py and plot.js files.

--- app.py
+++ app.py
@@ -3,8 +3,11 @@
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine, func
+from sqlalchemy import create_engine, func, desc
 from flask import Flask, jsonify
+
+import pandas as pandas
+import numpy as np
 
 # Set up the database.
 
@@ -20,7 +23,7 @@ Base.prepare(engine, reflect = True)
 
 OTU = Base.classes.otu
 Samples = Base.classes.samples
-Metadata = Base.Classes.samples_metadata
+Metadata = Base.classes.samples_metadata
 
 # Create our session (link) from Python to the DB
 session = Session(engine)
@@ -34,7 +37,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-	# Return the dashbord homepage.
+	return (f"Welcome to the Belly Button Biodiversity API!</br>"
+		    f"Available Routes:</br>"
+		    f"/api/v1.0/names</br>"
+		    f"/api/v1.0/otu</br>"
+		    f"/api/v1.0/metadata/<sample></br>"
+		    f"/api/v1.0/wfreq/<sample></br>"
+		    f"/api/1.0/sample/<sample></br>"
+		)
 
 @app.route("/names")
 #List of sample names.
@@ -46,36 +56,53 @@ def sample_names():
 #List of OTU descriptions.
 def otu_data():
 	results = session.query(OTU.otu_id, OTU.lowest_taxonomic_unit_found).order_by(OTU.otu_id).all()
-	otu_data = list(np.raval(results))
+	otu_data = list(np.ravel(results))
 	return jsonify(otu_data)
 
 @app.route("/metadata/<sample>")
 #Metadata for a given sample.
 def metadata_route(sample):
-    # sample = "BB_940"
-    num_sample = sample.replace("BB_", "")
-    metadata_sample = session.query(Metadata.AGE, Metadata.BBTYPE, Metadata.ETHNICITY, Metadata.GENDER, Metadata.LOCATION, Metadata.SAMPLEID).filter_by(SAMPLEID=num_sample).all()
-    record = metadata_sample[0]
-    metadata_dict = {"Age": record[0], 'BB_Type':record[1], 'Ethnicity': record[2],'Gender': record[3], 'Location': record[4], 'Sample_ID': record[5]}
-    return metadata_dict
+	# sample = "BB_940"
+	num_sample = sample.replace("BB_", "")
+	metadata_sample = session.query(Metadata.AGE, Metadata.BBTYPE, Metadata.ETHNICITY, Metadata.GENDER, Metadata.LOCATION, Metadata.SAMPLEID).filter_by(SAMPLEID=num_sample).all()
+	record = metadata_sample[0]
+	metadata_dict = {"Age": record[0], 'BB_Type':record[1], 'Ethnicity': record[2],'Gender': record[3], 'Location': record[4], 'Sample_ID': record[5]}
+	return metadata_dict
 
-sample_result = metadata_route("BB_940")
-return jsonify(sample_result)
+	sample_result = metadata_route("BB_940")
+	return jsonify(sample_result)
 
 @app.route("/wfreq/<sample>")
 #Weekly Washing frequency as a number.
 def wfreq_route(sample):
-    num_w_sample = sample.replace("BB_", "")
-    wfreq_sample = session.query(Metadata.SAMPLEID, Metadata.WFREQ).filter_by(SAMPLEID = num_w_sample).all()
-    w_record = wfreq_sample[0]
-    wfreq_dict = {'Sample ID' : w_record[0], 'Wash Frequency' : w_record[1]}
-    wfrequency = wfreq_dict['Wash Frequency']
-    return wfrequency
+	num_w_sample = sample.replace("BB_", "")
+	wfreq_sample = session.query(Metadata.SAMPLEID, Metadata.WFREQ).filter_by(SAMPLEID = num_w_sample).all()
+	w_record = wfreq_sample[0]
+	wfreq_dict = {'Sample ID' : w_record[0], 'Wash Frequency' : w_record[1]}
+	wfrequency = wfreq_dict['Wash Frequency']
+	return wfrequency
 
-wfreq_results = wfreq_route('BB_940')
-return jsonify(wfreq_results)
+	wfreq_results = wfreq_route('BB_940')
+	return jsonify(wfreq_results)
 
-if __nam__ == "__main__":
+@app.route("/sample/<sample>")
+def otu_sample_values(sample):
+    otu_ids = []
+    sample_values = []
+    sample_query = "Samples." + sample
+    results = session.query(Samples.otu_id, sample_query).order_by(desc(sample_query))
+    for result in range(len(results)):
+        otu_ids.append(results[0])
+    for result in range(len(results)):
+         sample_values.append(results[1])
+    return otu_ids
+    return sample_values
+    sample_values_dict = {'otu_id': otu_id, 'sample_values': sample_values}
+
+    return jsonify(samples_value_dict)
+
+
+if __name__ == "__main__":
 	app.run(debug=True)
 
 
--- plots.js
+++ plots.js
@@ -1,6 +1,48 @@
-function dropDownMenu() {
 
-	d3.selectAll(".sampleNames")
-		.append()
+// Create a dropdown menu for the name data.
 
+function selectMenu() {
+	var namesData = document.getElementById("seldataset");
+
+};
+
+function createOptions(selectMenu) {
+	var options = document.createElement("option");
+};
+
+//Create a pie chart for the OTU ID data.
+
+function buildOtuIdPieChart(data){
+	var trace1 = {
+		
+	}
 }
+
+// Create a bubble chart for the Sample Value versus the OTU ID.
+
+// Pull the data from the API.
+
+function getData() {
+
+	Plotly.d3.json("/names", function(error, data){
+		if (error) console.warn(error);
+	})
+
+	Plotly.d3.json("/otu", function(error, data){
+		if (error) console.warn(error);
+	})
+
+	Ploly.d3.json("/metadata/<sample>", function(error, data){
+		if (error) console.warn(error);
+	})
+
+	Plotly.d3.json("/wfreq/<sample>", function(error, data){
+		if (error) console.warn(error);
+	})
+
+	Plotly.d3.json("/sample/<sample>", function(error, data){
+		if (error) console.warn(error);
+	})
+}
+
+getData();
